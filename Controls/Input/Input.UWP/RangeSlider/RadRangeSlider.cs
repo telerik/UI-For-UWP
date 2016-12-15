af93db49ca389f0954dab789aa254d6b25f16d14
Vesselin Georgiev <vesselin.georgiev@telerik.com>
2016-12-15 17:49:26 +0200

    Added correct paths for RangeSlider

---
+++ Controls/Input/Input.UWP/RangeSlider/RadRangeSlider.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.UI.Xaml.Controls.Input.RangeSlider;
+using Telerik.UI.Xaml.Controls.Primitives;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Telerik.UI.Xaml.Controls.Input
+{
+    /// <summary>
+    /// Represents an input control that can be used for visual selection of a logical range. The selection range is visualized with scaled axes.
+    /// </summary>
+    public class RadRangeSlider : SliderBase
+    {
+        /// <summary>
+        /// Identifies the <see cref="TopLeftScaleStyle"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TopLeftScaleStyleProperty =
+          DependencyProperty.Register("TopLeftScaleStyle", typeof(Style), typeof(RadRangeSlider), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="BottomRightScaleStyle"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty BottomRightScaleStyleProperty =
+          DependencyProperty.Register("BottomRightScaleStyle", typeof(Style), typeof(RadRangeSlider), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="SliderPrimitiveStyle"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SliderPrimitiveStyleProperty =
+          DependencyProperty.Register("SliderPrimitiveStyle", typeof(Style), typeof(RadRangeSlider), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Identifies the <see cref="LabelFormat"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelFormatProperty =
+          DependencyProperty.Register("LabelFormat", typeof(string), typeof(RadRangeSlider), new PropertyMetadata(null));
+
+        private const string RangeSliderPanelName = "PART_RangeSliderPanel";
+        private const string RangeSliderPrimitivePartName = "PART_RangeSliderPrimitive";
+        private const string TopLeftScalePartName = "PART_ScaleTopLeft";
+        private const string BottomRightScalePartName = "PART_ScaleBottomRight";
+
+        private RangeSliderPanel rangeSliderPanel;
+        private RangeSliderPrimitive rangeSlider;
+        private ScalePrimitive scaleTopLeft;
+        private ScalePrimitive scaleBottomRight;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RadRangeSlider"/> class.
+        /// </summary>
+        public RadRangeSlider()
+        {
+            this.DefaultStyleKey = typeof(RadRangeSlider);
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="SliderPrimitiveStyle"/> that defines the appearance settings applied to the inner RangeSliderPrimitive control.
+        /// </summary>
+        /// <remarks>
+        /// The style should target the <see cref="Telerik.UI.Xaml.Controls.Primitives.RangeSliderPrimitive"/> type.
+        /// </remarks>
+        /// <example>
+        /// <para>THis example demonstrates how to set a custom style to the <see cref="RangeSliderPrimitive"/> control.</para>
+        /// <para>The following namespaces have to be added to the page:</para>
+        /// <code language="xaml">
+        /// xmlns:telerikInput="using:Telerik.UI.Xaml.Controls.Input"
+        /// xmlns:telerikPrimitives="using:Telerik.UI.Xaml.Controls.Primitives"
+        /// </code>
+        /// <para>Here is how you change the style of the <see cref="RangeSliderPrimitive.SelectionStartThumb"/>:</para>
+        /// <code language="xaml">
+        /// &lt;telerikInput:RadRangeSlider &gt;
+        ///     &lt;telerikInput:RadRangeSlider.SliderPrimitiveStyle&gt;
+        ///         &lt;Style TargetType="telerikPrimitives:RangeSliderPrimitive"&gt;
+        ///             &lt;!-- set the RangeSliderPrimitive properties here --&gt;
+        ///         &lt;/Style&gt;
+        ///     &lt;/telerikInput:RadRangeSlider.SliderPrimitiveStyle&gt;
+        /// &lt;/telerikInput:RadRangeSlider&gt;
+        /// </code>
+        /// </example>
+        public Style SliderPrimitiveStyle
+        {
+            get
+            {
+                return (Style)this.GetValue(SliderPrimitiveStyleProperty);
+            }
+            set
+            {
+                this.SetValue(SliderPrimitiveStyleProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="TopLeftScaleStyle"/> that defines the appearance settings applied to the top/left Scale control.
+        /// </summary>
+        /// <remarks>
+        /// The style should target the <see cref="Telerik.UI.Xaml.Controls.Primitives.ScalePrimitive"/> type.
+        /// </remarks>
+        /// <example>
+        /// <code language="xaml">
+        /// &lt;telerikInput:RadRangeSlider&gt;
+        ///     &lt;telerikInput:RadRangeSlider.TopLeftScaleStyle&gt;
+        ///         &lt;Style TargetType="telerikPrimitives:ScalePrimitive"&gt;
+        ///             &lt;Setter Property="LabelPlacement" Value="None"/&gt;
+        ///             &lt;Setter Property="TickPlacement" Value="BottomRight"/&gt;
+        ///             &lt;Setter Property="TickTemplate"&gt;
+        ///                 &lt;Setter.Value&gt;
+        ///                     &lt;DataTemplate&gt;
+        ///                         &lt;Ellipse Width="5" Height="5" Margin="-2.5,0,0,10" Fill="LimeGreen"/&gt;
+        ///                     &lt;/DataTemplate&gt;
+        ///                 &lt;/Setter.Value&gt;
+        ///             &lt;/Setter&gt;
+        ///         &lt;/Style&gt;
+        ///     &lt;/telerikInput:RadRangeSlider.TopLeftScaleStyle&gt;
+        /// &lt;/telerikInput:RadRangeSlider&gt;
+        /// </code>
+        /// </example>
+        public Style TopLeftScaleStyle
+        {
+            get
+            {
+                return (Style)this.GetValue(TopLeftScaleStyleProperty);
+            }
+            set
+            {
+                this.SetValue(TopLeftScaleStyleProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="BottomRightScaleStyle"/> that defines the appearance settings applied to the bottom/right Scale control.
+        /// </summary>
+        /// <remarks>
+        /// The style should target the <see cref="Telerik.UI.Xaml.Controls.Primitives.ScalePrimitive"/> type.
+        /// </remarks>
+        /// <example>
+        /// <code language="xaml">
+        /// &lt;telerikInput:RadRangeSlider&gt;
+        ///     &lt;telerikInput:RadRangeSlider.BottomRightScaleStyle&gt;
+        ///         &lt;Style TargetType="telerikPrimitives:ScalePrimitive"&gt;
+        ///             &lt;Setter Property="LabelPlacement" Value="BottomRight"/&gt;
+        ///             &lt;Setter Property="TickPlacement" Value="TopLeft"/&gt;
+        ///             &lt;Setter Property="TickTemplate"&gt;
+        ///                 &lt;Setter.Value&gt;
+        ///                     &lt;DataTemplate&gt;
+        ///                         &lt;Ellipse Width="5" Height="5" Margin="-2.5,10,0,0" Fill="LimeGreen"/&gt;
+        ///                     &lt;/DataTemplate&gt;
+        ///                 &lt;/Setter.Value&gt;
+        ///             &lt;/Setter&gt;
+        ///         &lt;/Style&gt;
+        ///     &lt;/telerikInput:RadRangeSlider.BottomRightScaleStyle&gt;
+        /// &lt;/telerikInput:RadRangeSlider&gt;
+        /// </code>
+        /// </example>
+        public Style BottomRightScaleStyle
+        {
+            get
+            {
+                return (Style)this.GetValue(BottomRightScaleStyleProperty);
+            }
+            set
+            {
+                this.SetValue(BottomRightScaleStyleProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the string that is used to format the labels of the component. 
+        /// The value passed should follow the syntax, expected by the runtime 
+        /// String.Format: { index[,alignment][:formatString] }.
+        /// </summary>
+        /// <example>
+        /// <code language="xaml">
+        /// &lt;telerikInput:RadRangeSlider Minimum="0" Maximum="1" LabelFormat="{}{0:P0}"/&gt;
+        /// </code>
+        /// </example>
+        public string LabelFormat
+        {
+            get
+            {
+                return (string)this.GetValue(LabelFormatProperty);
+            }
+            set
+            {
+                this.SetValue(LabelFormatProperty, value);
+            }
+        }
+
+        internal RangeSliderPrimitive RangeSlider
+        {
+            get
+            {
+                return this.rangeSlider;
+            }
+        }
+
+        internal ScalePrimitive ScaleTopLeft
+        {
+            get
+            {
+                return this.scaleTopLeft;
+            }
+        }
+
+        internal ScalePrimitive ScaleBottomRight
+        {
+            get
+            {
+                return this.scaleBottomRight;
+            }
+        }
+
+        internal override void OnSelectionStartChanged(double oldSelectionStart, double newSelectionStart)
+        {
+            base.OnSelectionStartChanged(oldSelectionStart, newSelectionStart);
+            if (!this.IsTemplateApplied)
+            {
+                return;
+            }
+
+            double calculatedSelectionStart = RangeSliderPrimitive.CalculateSelectionStart(this.Minimum, this.Maximum, this.SelectionStart, this.SelectionStart);
+            this.SelectionStart = this.rangeSlider.Snap(calculatedSelectionStart);
+        }
+
+        internal override void OnSelectionEndChanged(double oldSelectionEnd, double newSelectionEnd)
+        {
+            base.OnSelectionEndChanged(oldSelectionEnd, newSelectionEnd);
+            if (!this.IsTemplateApplied)
+            {
+                return;
+            }
+
+            double calculatedSelectionEnd = this.rangeSlider.CalculateSelectionEnd(this.Maximum, this.SelectionStart, this.SelectionEnd, this.SelectionEnd);
+            this.SelectionEnd = this.rangeSlider.Snap(calculatedSelectionEnd);
+        }
+
+        /// <summary>
+        /// Called when the Framework <see cref="M:RadControl.OnApplyTemplate" /> is called. Inheritors should override this method should they have some custom template-related logic.
+        /// This is done to ensure that the <see cref="P:IsTemplateApplied" /> property is properly initialized.
+        /// </summary>
+        protected override bool ApplyTemplateCore()
+        {
+            bool applied = base.ApplyTemplateCore();
+
+            this.rangeSliderPanel = this.GetTemplatePartField<RangeSliderPanel>(RangeSliderPanelName);
+            applied = applied && this.rangeSliderPanel != null;
+
+            this.rangeSlider = this.GetTemplatePartField<RangeSliderPrimitive>(RangeSliderPrimitivePartName);
+            applied = applied && this.rangeSlider != null;
+
+            this.scaleTopLeft = this.GetTemplatePartField<ScalePrimitive>(TopLeftScalePartName);
+            applied = applied && this.scaleTopLeft != null;
+
+            this.scaleBottomRight = this.GetTemplatePartField<ScalePrimitive>(BottomRightScalePartName);
+            applied = applied && this.scaleBottomRight != null;
+
+            return applied;
+        }
+
+        /// <summary>
+        /// Occurs when the <see cref="M:OnApplyTemplate" /> method has been called and the template is already successfully applied.
+        /// </summary>
+        protected override void OnTemplateApplied()
+        {
+            base.OnTemplateApplied();
+
+            this.rangeSlider.SelectionOffsetChanged += this.OnSelectionOffsetChanged;
+
+            this.rangeSliderPanel.Owner = this;
+        }
+
+        /// <inheritdoc/>
+        protected override void UnapplyTemplateCore()
+        {
+            base.UnapplyTemplateCore();
+
+            this.rangeSlider.SelectionOffsetChanged -= this.OnSelectionOffsetChanged;
+        }
+
+        private void OnSelectionOffsetChanged(object sender, EventArgs e)
+        {
+            this.rangeSliderPanel.InvalidateMeasure();
+        }
+    }
+}
---
+++ Controls/Input/Input.UWP/RangeSlider/RangeSliderPanel.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Telerik.UI.Xaml.Controls.Input.RangeSlider
+{
+    /// <summary>
+    /// Represents a custom panel which hosts the RadRangeSlider controls (ScalePrimitive/RangeSliderPrimitive) and arranges them in the desired layout.
+    /// </summary>
+    public class RangeSliderPanel : Panel
+    {
+        private RadRangeSlider owner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RangeSliderPanel"/> class.
+        /// </summary>
+        public RangeSliderPanel()
+        {
+        }
+
+        internal RadRangeSlider Owner
+        {
+            get
+            {
+                return this.owner;
+            }
+            set
+            {
+                this.owner = value;
+            }
+        }
+
+        /// <summary>
+        /// Called in the measure layout pass to determine the desired size.
+        /// </summary>
+        /// <param name="availableSize">The available size that was given by the layout system.</param>
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            base.MeasureOverride(availableSize);
+            Size finalSize = new Size();
+
+            this.Owner.ScaleTopLeft.Measure(availableSize);
+            this.Owner.RangeSlider.Measure(availableSize);
+            this.Owner.ScaleBottomRight.Measure(availableSize);
+
+            if (this.Owner.Orientation == Orientation.Horizontal)
+            {
+                finalSize.Height = this.Owner.ScaleTopLeft.DesiredSize.Height;
+                finalSize.Height += this.Owner.ScaleBottomRight.DesiredSize.Height;
+                finalSize.Height += this.Owner.RangeSlider.DesiredSize.Height;
+
+                double maxWidth = Math.Max(this.Owner.ScaleTopLeft.DesiredSize.Width, this.Owner.RangeSlider.DesiredSize.Width);
+                maxWidth = Math.Max(maxWidth, this.Owner.ScaleBottomRight.DesiredSize.Width);
+                finalSize.Width = maxWidth;
+            }
+            else
+            {
+                finalSize.Width = this.Owner.ScaleTopLeft.DesiredSize.Width;
+                finalSize.Width += this.Owner.ScaleBottomRight.DesiredSize.Width;
+                finalSize.Width += this.Owner.RangeSlider.DesiredSize.Width;
+
+                double maxHeight = Math.Max(this.Owner.ScaleTopLeft.DesiredSize.Height, this.Owner.RangeSlider.DesiredSize.Height);
+                maxHeight = Math.Max(maxHeight, this.Owner.ScaleBottomRight.DesiredSize.Height);
+                finalSize.Height = maxHeight;
+
+                if (finalSize.Height > availableSize.Height)
+                {
+                    finalSize.Height = availableSize.Height;
+                }
+            }
+
+            return finalSize;
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Arrange pass of layout. Classes can override this method to define their own Arrange pass behavior.
+        /// </summary>
+        /// <param name="finalSize">The final area within the parent that this object should use to arrange itself and its children.</param>
+        /// <returns>
+        /// The actual size that is used after the element is arranged in layout.
+        /// </returns>
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            Size size = base.ArrangeOverride(finalSize);
+
+            if (this.owner.Orientation == Orientation.Horizontal)
+            {
+                this.ArrangeHorizontal(finalSize);
+            }
+            else
+            {
+                this.ArrangeVertical(finalSize);
+            }
+
+            return size;
+        }
+
+        private void ArrangeHorizontal(Size finalSize)
+        {
+            double leftOffset = Math.Max(Math.Max(this.Owner.RangeSlider.SelectionStartOffset, this.Owner.ScaleTopLeft.AxisLineOffset.Left), this.Owner.ScaleBottomRight.AxisLineOffset.Left);
+
+            double rightOffset = Math.Max(Math.Max(this.Owner.RangeSlider.SelectionEndOffset, this.Owner.ScaleTopLeft.AxisLineOffset.Right), this.Owner.ScaleBottomRight.AxisLineOffset.Right);
+            double top = 0;
+            double scaleTickMargin = 1;
+
+            double left = leftOffset - this.Owner.ScaleTopLeft.AxisLineOffset.Left;
+            double scaleRightMargin = rightOffset - this.Owner.ScaleTopLeft.AxisLineOffset.Right;
+            double width = Math.Max(finalSize.Width - left - scaleRightMargin, 0);
+            double height = Math.Min(this.Owner.ScaleTopLeft.DesiredSize.Height, finalSize.Height);
+
+            this.Owner.ScaleTopLeft.Arrange(new Rect(left, top - scaleTickMargin, width, height));
+            top = height;
+
+            left = 0;
+            width = finalSize.Width;
+
+            if (this.owner.RangeSlider.SelectionStartOffset < leftOffset)
+            {
+                left = leftOffset - this.owner.RangeSlider.SelectionStartOffset;
+                width -= left;
+            }
+
+            if (this.owner.RangeSlider.SelectionEndOffset < rightOffset)
+            {
+                width -= rightOffset - this.owner.RangeSlider.SelectionEndOffset;
+            }
+
+            height = Math.Min(this.Owner.RangeSlider.DesiredSize.Height, finalSize.Height - top);
+
+            this.Owner.RangeSlider.Arrange(new Rect(left, top, width, height));
+
+            top += height;
+
+            left = leftOffset - this.Owner.ScaleBottomRight.AxisLineOffset.Left;
+            scaleRightMargin = rightOffset - this.Owner.ScaleBottomRight.AxisLineOffset.Right;
+            width = Math.Max(finalSize.Width - left - scaleRightMargin, 0);
+            height = Math.Min(this.Owner.ScaleBottomRight.DesiredSize.Height, finalSize.Height - top);
+            this.Owner.ScaleBottomRight.Arrange(new Rect(left, top + scaleTickMargin, width, height));
+        }
+
+        private void ArrangeVertical(Size finalSize)
+        {
+            double topOffset = Math.Max(Math.Max(this.Owner.RangeSlider.SelectionEndOffset, this.Owner.ScaleTopLeft.AxisLineOffset.Right), this.Owner.ScaleBottomRight.AxisLineOffset.Right);
+
+            double bottomOffset = Math.Max(Math.Max(this.Owner.RangeSlider.SelectionStartOffset, this.Owner.ScaleTopLeft.AxisLineOffset.Left), this.Owner.ScaleBottomRight.AxisLineOffset.Left);
+
+            double leftOffset = 0;
+            double scaleTickMargin = 1;
+
+            double top = topOffset - this.Owner.ScaleTopLeft.AxisLineOffset.Top;
+            double scaleBottomMargin = bottomOffset - this.Owner.ScaleTopLeft.AxisLineOffset.Bottom;
+            double height = Math.Max(finalSize.Height - top - scaleBottomMargin, 0);
+            double width = Math.Min(this.Owner.ScaleTopLeft.DesiredSize.Width, finalSize.Width);
+            this.Owner.ScaleTopLeft.Arrange(new Rect(leftOffset - scaleTickMargin, top, width, height));
+
+            leftOffset += width;
+
+            top = 0;
+            height = finalSize.Height;
+
+            if (this.owner.RangeSlider.SelectionStartOffset < bottomOffset)
+            {
+                top = bottomOffset - this.owner.RangeSlider.SelectionStartOffset;
+                height -= top;
+            }
+
+            if (this.owner.RangeSlider.SelectionEndOffset < topOffset)
+            {
+                height -= topOffset - this.owner.RangeSlider.SelectionEndOffset;
+            }
+
+            width = Math.Min(this.Owner.RangeSlider.DesiredSize.Width, finalSize.Width);
+            this.Owner.RangeSlider.Arrange(new Rect(leftOffset, top, width, height));
+
+            leftOffset += width;
+
+            top = topOffset - this.Owner.ScaleBottomRight.AxisLineOffset.Top;
+            scaleBottomMargin = bottomOffset - this.Owner.ScaleBottomRight.AxisLineOffset.Bottom;
+            height = Math.Max(finalSize.Height - top - scaleBottomMargin, 0);
+            width = Math.Min(this.Owner.ScaleBottomRight.DesiredSize.Width, finalSize.Width);
+            this.Owner.ScaleBottomRight.Arrange(new Rect(leftOffset + scaleTickMargin, top, width, height));
+        }
+    }
+}
