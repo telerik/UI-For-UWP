3fee125eb7466d8c4884d5a5e3805bd9298f9ad9
APopatanasov <atanas.popatanasov@telerik.com>
2018-01-15 14:48:07 +0200

    Added SDK example that demonstrates creation of custom column.

---
+++ SDKExamples.UWP/Examples/Grid/CustomRatingColumn/CustomFilteringControl/DataGridCustomFilteringControl.cs
@@ -0,0 +1,56 @@
+﻿using Telerik.Data.Core;
+using Telerik.UI.Xaml.Controls.Grid.Primitives;
+using Telerik.UI.Xaml.Controls.Input;
+using Windows.UI.Xaml;
+
+namespace SDKExamples.UWP.DataGrid
+{
+    [TemplatePart(Name = "PART_Rating", Type = typeof(RadRating))]
+    public sealed class DataGridCustomFilteringControl : DataGridFilterControlBase
+    {
+        private RadRating rating;
+
+        public DataGridCustomFilteringControl()
+        {
+            this.DefaultStyleKey = typeof(DataGridCustomFilteringControl);
+        }
+
+        public string PropertyName
+        {
+            get;
+            set;
+        }
+
+        public override FilterDescriptorBase BuildDescriptor()
+        {
+            var descriptor = new NumericalFilterDescriptor();
+            descriptor.PropertyName = this.PropertyName;
+            descriptor.Value = this.rating.Value;
+
+            return descriptor;
+        }
+
+        protected override bool ApplyTemplateCore()
+        {
+            bool applied = base.ApplyTemplateCore();
+
+            this.rating = this.GetTemplateChild("PART_Rating") as RadRating;
+            applied = applied && this.rating != null;
+
+            return applied;
+        }
+
+        protected override void Initialize()
+        {
+            var descriptor = this.ActualAssociatedDescriptor as NumericalFilterDescriptor;
+            if (descriptor != null)
+            {
+                double value;
+                if (double.TryParse(descriptor.Value.ToString(), out value))
+                {
+                    this.rating.Value = value;
+                }
+            }
+        }
+    }
+}
---
+++ SDKExamples.UWP/Examples/Grid/CustomRatingColumn/CustomRatingColumn.xaml.cs
@@ -0,0 +1,16 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SDKExamples.UWP.DataGrid
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CustomRatingColumn : ExamplePageBase
+    {
+        public CustomRatingColumn()
+        {
+            this.InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+    }
+}
