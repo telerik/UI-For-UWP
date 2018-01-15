feb11c865401e7ae7448eead061cfd31299c50b4
APopatanasov <atanas.popatanasov@telerik.com>
2018-01-15 11:16:44 +0200

    Updated version.

--- SDKExamples.UWP/Properties/AssemblyInfo.cs
+++ SDKExamples.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SDKExamples.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2015-2018 Telerik AD.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- UnitTests/UAP.Tests/Input.Tests/Properties/AssemblyInfo.cs
+++ UnitTests/UAP.Tests/Input.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Input.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2015-2018 Telerik AD.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyMetadata("TargetPlatform", "UAP")]
