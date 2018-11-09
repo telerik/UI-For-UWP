f4fb8451cba99b41260ab00112dfa2d09c5121f8
ni4ka7a <n.atanasow@yahoo.com>
2018-11-09 15:04:25 +0200

    Extract GetFirstDateToRender in the calendar model so it can be executed even before the calendar ViewModels are loaded

--- Controls/Input/Input.UWP/Calendar/Model/CalendarYearViewModel.cs
+++ Controls/Input/Input.UWP/Calendar/Model/CalendarYearViewModel.cs
@@ -25,7 +25,7 @@ namespace Telerik.UI.Xaml.Controls.Input.Calendar
 
         internal override DateTime GetFirstDateToRender(DateTime date)
         {
-            return CalendarMathHelper.GetFirstDateOfYear(date);
+            return this.Calendar.GetFirstDateToRenderForDisplayMode(date, CalendarDisplayMode.YearView);
         }
 
         internal override DateTime GetNextDateToRender(DateTime date)
