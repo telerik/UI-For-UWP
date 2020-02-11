98fc0d51f8b412af57bd1f5afa320636e1de76d0
atanas.popatanasov <atanas.popatanasov@progress.com>
2020-02-11 15:34:20 +0200

    Improve the logic for checking which data points should be rendered in the viewport.

--- Controls/Chart/Chart.UWP/Visualization/Common/Renderers/ChartSeriesRenderer.cs
+++ Controls/Chart/Chart.UWP/Visualization/Common/Renderers/ChartSeriesRenderer.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using Telerik.Charting;
-using Telerik.Core;
+using Windows.Foundation;
 using Windows.UI.Composition;
 using Windows.UI.Xaml.Media;
 
@@ -10,6 +10,7 @@ namespace Telerik.UI.Xaml.Controls.Chart
     {
         internal ChartSeriesModel model;
         internal IList<DataPoint> renderPoints;
+        private const double PlotAreaVicinity = 5.0;
 
         public void Render(bool isDrawnWithComposition = false)
         {
@@ -90,25 +91,6 @@ namespace Telerik.UI.Xaml.Controls.Chart
             return this.GetRenderPoints(this.model.DataPointsInternal);
         }
 
-        protected virtual bool ShouldPlotPoint(RadRect point)
-        {
-            var plotBounds = this.model.layoutSlot;
-
-            var isWithinXPlot = point.X <= plotBounds.X + plotBounds.Width && point.X + point.Width >= plotBounds.X;
-            var isWithinYPlot = point.Y <= plotBounds.Y + plotBounds.Height && point.Y + point.Height >= plotBounds.Y;
-
-            if (isWithinXPlot && isWithinYPlot)
-            {
-                return true;
-            }
-
-            var plotDirection = this.model.GetTypedValue<AxisPlotDirection>(AxisModel.PlotDirectionPropertyKey, AxisPlotDirection.Vertical);
-            
-            // empty values
-            return (isWithinXPlot && double.IsNaN(point.Y) && plotDirection == AxisPlotDirection.Vertical) ||
-                   (isWithinYPlot && double.IsNaN(point.X) && plotDirection == AxisPlotDirection.Horizontal);
-        }
-
         protected Brush GetPaletteBrush(PaletteVisualPart part)
         {
             ChartSeries series = this.model.presenter as ChartSeries;
@@ -124,63 +106,87 @@ namespace Telerik.UI.Xaml.Controls.Chart
 
         protected abstract void RenderCore();
 
-        private static bool LineIntersectsRect(RadPoint firstPoint, RadPoint secondPoint, RadRect rect)
+        private List<DataPoint> GetRenderPoints(IList<DataPoint> dataPoints)
         {
-            return LineIntersectsLine(firstPoint, secondPoint, new RadPoint(rect.X, rect.Y), new RadPoint(rect.X + rect.Width, rect.Y)) ||
-                LineIntersectsLine(firstPoint, secondPoint, new RadPoint(rect.X + rect.Width, rect.Y), new RadPoint(rect.X + rect.Width, rect.Y + rect.Height)) ||
-                LineIntersectsLine(firstPoint, secondPoint, new RadPoint(rect.X + rect.Width, rect.Y + rect.Height), new RadPoint(rect.X, rect.Y + rect.Height)) ||
-                LineIntersectsLine(firstPoint, secondPoint, new RadPoint(rect.X, rect.Y + rect.Height), new RadPoint(rect.X, rect.Y)) ||
-                (rect.Contains(firstPoint.X, firstPoint.Y) && rect.Contains(secondPoint.X, secondPoint.Y));
-        }
+            var clipRect = this.model.layoutSlot;
+            var plotDirection = this.model.GetTypedValue<AxisPlotDirection>(AxisModel.PlotDirectionPropertyKey, AxisPlotDirection.Vertical);
+            bool isVertical = plotDirection == AxisPlotDirection.Vertical;
+            double start = isVertical ? clipRect.X - PlotAreaVicinity : clipRect.Y - PlotAreaVicinity;
+            double end = isVertical ? clipRect.Right + PlotAreaVicinity : clipRect.Bottom + PlotAreaVicinity;
 
-        private static bool LineIntersectsLine(RadPoint firstLineFirstPoint, RadPoint firstLineSecondPoint, RadPoint secondLineFirstPoint, RadPoint secondLineSecondPoint)
-        {
-            var d = (firstLineSecondPoint.X - firstLineFirstPoint.X) * (secondLineSecondPoint.Y - secondLineFirstPoint.Y) - (firstLineSecondPoint.Y - firstLineFirstPoint.Y) * (secondLineSecondPoint.X - secondLineFirstPoint.X);
-            if (d == 0)
-            {
-                return false;
-            }
+            bool addedPreviousPoint = false;
+            Point? previousPoint = null;
+            DataPoint previousDataPoint = null;
+            bool isPreviousPointInView = false;
 
-            var q = (firstLineFirstPoint.Y - secondLineFirstPoint.Y) * (secondLineSecondPoint.X - secondLineFirstPoint.X) - (firstLineFirstPoint.X - secondLineFirstPoint.X) * (secondLineSecondPoint.Y - secondLineFirstPoint.Y);
-            var r = q / d;
-
-            q = (firstLineFirstPoint.Y - secondLineFirstPoint.Y) * (firstLineSecondPoint.X - firstLineFirstPoint.X) - (firstLineFirstPoint.X - secondLineFirstPoint.X) * (firstLineSecondPoint.Y - firstLineFirstPoint.Y);
-            var s = q / d;
-            if (r < 0 || r > 1 || s < 0 || s > 1)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private List<DataPoint> GetRenderPoints(IList<DataPoint> points)
-        {
             var renderPoints = new List<DataPoint>();
-            for (int i = 0; i < points.Count; i++)
+            for (int i = 0; i < dataPoints.Count; i++)
             {
-                var point = points[i];
-                var pointBounds = RadRect.Round(point.layoutSlot);
-                if (this.ShouldPlotPoint(pointBounds))
+                var dataPoint = dataPoints[i];
+                var point = dataPoint.Center();
+                if (dataPoints[i].isEmpty || double.IsNaN(point.X) || double.IsNaN(point.Y))
                 {
-                    renderPoints.Add(point);
+                    // Point is empty
+                    addedPreviousPoint = false;
+                    previousPoint = null;
+                    previousDataPoint = null;
+                    isPreviousPointInView = false;
                     continue;
                 }
 
-                RadRect nextPointBounds = RadRect.Empty;
-                if (i + 1 < points.Count)
+                double position = isVertical ? point.X : point.Y;
+                if (start <= position && position <= end)
                 {
-                    nextPointBounds = RadRect.Round(points[i + 1].layoutSlot);
+                    // Point is inside the viewport
+                    if (!addedPreviousPoint && previousPoint.HasValue)
+                    {
+                        renderPoints.Add(previousDataPoint);
+                    }
+
+                    renderPoints.Add(dataPoint);
+                    addedPreviousPoint = true;
+                    isPreviousPointInView = true;
                 }
-                else if (i - 1 > -1)
+                else
                 {
-                    nextPointBounds = RadRect.Round(points[i - 1].layoutSlot);
+                    if (isPreviousPointInView)
+                    {
+                        // Point is not inside the viewport, but the previous point is
+                        renderPoints.Add(dataPoint);
+                        addedPreviousPoint = true;
+                    }
+                    else
+                    {
+                        // Both the current point and the previous point are not in the viewport
+                        bool lineIntersectsClipRect = false;
+                        if (previousPoint.HasValue)
+                        {
+                            double prevPosition = isVertical ? previousPoint.Value.X : previousPoint.Value.Y;
+                            position = isVertical ? point.X : point.Y;
+                            lineIntersectsClipRect = (prevPosition <= start && end <= position) || (position <= start && end <= prevPosition);
+                        }
+
+                        if (!addedPreviousPoint && lineIntersectsClipRect)
+                        {
+                            renderPoints.Add(previousDataPoint);
+                        }
+
+                        if (lineIntersectsClipRect)
+                        {
+                            renderPoints.Add(dataPoint);
+                            addedPreviousPoint = true;
+                        }
+                        else
+                        {
+                            addedPreviousPoint = false;
+                        }
+                    }
+
+                    isPreviousPointInView = false;
                 }
 
-                if (LineIntersectsRect(pointBounds.Location, nextPointBounds.Location, this.model.layoutSlot))
-                {
-                    renderPoints.Add(points[i]);
-                }
+                previousPoint = point;
+                previousDataPoint = dataPoint;
             }
 
             return renderPoints;
